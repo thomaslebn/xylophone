f484c6ebd8493eeee837a6c5eed23b4d327225f9
thomaslebn <thomas.lebn@protonmail.com>
2022-01-23 17:10:25 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(int n) {
+    final player = AudioCache();
+    player.play('note$n.wav');
+  }
+
+  Expanded buildKey(Color color, int n) {
+    return Expanded(
+      child: MaterialButton(
+        color: color,
+        onPressed: () {
+          playSound(n);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.lightGreen, 4),
+              buildKey(Colors.teal, 5),
+              buildKey(Colors.blue, 6),
+              buildKey(Colors.purple, 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
